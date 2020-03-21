659b769d37f2b689cb69f0c43987c8c30005ead9
Thomas Pope <tpope0928@gmail.com>
2020-03-21 21:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // Write your code here!
-let element = document.createElement('div')
+let header = document.getElementById("div#header");
+header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
